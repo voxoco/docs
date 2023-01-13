ebdc1ed3ff68d083855d7c1ceda345b16e0d1864
bryan-justin <bryanjustinde@gmail.com>
2023-01-13 08:13:22 -0500

    Added main page

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
   const url = request.nextUrl.clone()
   if (url.pathname === '/') {
-    url.pathname = '/voxo-public/release-notes/whats-new'
+    url.pathname = '/voxo-public'
     return NextResponse.redirect(url)
   }
 }
